d9f6f2b6e179c6fe0e7b5939ab661046a3742c40
Stephen <stephen@behnfeldt.pro>
2024-05-13 06:55:34 -0400

    Added first twig template

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class AppController
+class AppController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Check</strong>');
+        $netWorth = 4_200_000;
+        return $this->render('app/homepage.html.twig', [ 'total' => $netWorth]);
     }
 }
